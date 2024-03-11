29248d3fca57a74f8ee7ef1e94a7eff0fd7f14fb
Rodion Yeroshek <yeroshek@gmail.com>
2024-03-11 15:37:18 +0000

    Removed more stuff

--- opencv.py
+++ opencv.py
@@ -12,33 +12,6 @@ app = Flask(__name__)
 
 orientation = 180 # 0, 90, 180, 270
 
-
-def increase_luminance_contrast(frame):
-    lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
-    l_channel, a, b = cv2.split(lab)
-
-    # Applying CLAHE to L-channel
-    # feel free to try different values for the limit and grid size:
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    cl = clahe.apply(l_channel)
-
-    # merge the CLAHE enhanced L-channel with the a and b channel
-    limg = cv2.merge((cl, a, b))
-    frame = enhanced_img = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
-    return frame
-
-def rotatate_coordinate(pos, shape, orientation):
-    x, y = pos
-    len_x, len_y = shape
-    if orientation == 0:
-        return x, y
-    elif orientation == 90:
-        return y, len_x - x
-    elif orientation == 180:
-        return len_x - x, len_y - y
-    elif orientation == 270:
-        return len_y - y, x
-
 def rotate_frame(frame, orientation):
     if orientation == 0:
         return frame
@@ -74,45 +47,15 @@ def draw_rectangle_around_brightest(frame):
 
     return frame
 
-class FpsCounter:
-    def __init__(self, alpha=0.9, init_frame_count=10):
-        self.alpha = alpha
-        self.init_frame_count = init_frame_count
-        self.frame_times = []
-        self.start_time = time.time()
-        self.ema_duration = None
-
-    def update(self):
-        current_time = time.time()
-        frame_duration = current_time - self.start_time
-
-        if len(self.frame_times) < self.init_frame_count:
-            self.frame_times.append(frame_duration)
-            self.ema_duration = sum(self.frame_times) / len(self.frame_times)
-        else:
-            self.ema_duration = (
-                self.alpha * self.ema_duration + (1.0 - self.alpha) * frame_duration
-            )
-
-        self.start_time = current_time
-
-    def get_fps(self):
-        if self.ema_duration is not None:
-            return 1.0 / self.ema_duration
-        else:
-            return None
-
 
 def generate_frames():
 
     cap = ht301_hacklib.HT301()
 
-    fps_counter = FpsCounter(alpha=0.8)
     upscale_factor = 1
 
     while True:
         ret, frame = cap.read()
-        fps_counter.update()
         shape = frame.shape[0]
         frame = frame.astype(np.float32)
 
@@ -123,15 +66,13 @@ def generate_frames():
 
         frame = cv2.applyColorMap(frame, cv2.COLORMAP_INFERNO)
 
-        # frame = increase_luminance_contrast(frame)
-
         frame = rotate_frame(frame, orientation)
 
         frame = draw_rectangle_around_brightest(frame)
 
-        # frame = np.kron(frame, np.ones((upscale_factor, upscale_factor, 1))).astype(
-        #     np.uint8
-        # )
+        frame = np.kron(frame, np.ones((upscale_factor, upscale_factor, 1))).astype(
+            np.uint8
+        )
 
         # Web
         _, buffer = cv2.imencode('.jpg', frame)
