d9f5ae154cea70507378f6beb9e54c4ca9aa73b9
Rodion Yeroshek <yeroshek@gmail.com>
2024-04-13 15:24:40 +0100

    Better video split by file

--- src/video_capture.py
+++ src/video_capture.py
@@ -3,13 +3,16 @@ import time
 import os
 from datetime import datetime
 
-def capture_video(frames, video_source=0, save_dir='./', video_length=15*60, fps=30, write_interval=2):
+SAVE_DIR = '../videos/'
+
+def capture_video(frames, video_source=0, video_length=5, fps=30):
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # video codec
+    new_file_started = False
 
     def create_new_video_file(frame_size):
         timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')  # Get current time in the format YYYYMMDD_HHMMSS
-        video_filename = os.path.join(save_dir, f'video_{timestamp}.mp4')
-        return cv2.VideoWriter(video_filename, fourcc, fps, frame_size)
+        video_filename = os.path.join(SAVE_DIR, f'video_{timestamp}.mp4')
+        return cv2.VideoWriter(video_filename, fourcc, fps, frame_size), video_filename
 
     while True:
         try:
@@ -22,26 +25,26 @@ def capture_video(frames, video_source=0, save_dir='./', video_length=15*60, fps
 
     frame_size = (frame.shape[1], frame.shape[0])
     start_time = time.time()
-    write_time = time.time()
 
     print(f'Frame size: {frame_size}')
 
-    out = create_new_video_file(frame_size)
+    out, video_filename = create_new_video_file(frame_size)
 
     while True:
         frame = frames[video_source]
         if frame is None:
             continue
 
-        if time.time() - start_time >= video_length:
-            out.release()
-            start_time = time.time()
-            out = create_new_video_file(frame_size)
+        out.write(frame)
 
-        if time.time() - write_time >= write_interval:
-            out.write(frame)
-            write_time = time.time()
-            print(f'Writing frame to video file: {write_time}')
+        current_time = datetime.now()
+        if current_time.minute % video_length == 0 and current_time.second == 0:
+            if not new_file_started:
+                out.release()
+                out, video_filename = create_new_video_file(frame_size)
+                new_file_started = True
+        else:
+            new_file_started = False
 
     out.release()
     cv2.destroyAllWindows()
